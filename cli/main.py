f96f28fddc34e2d32d4fd8704575a18ea1b70ba7
iandioch <iandioch11@gmail.com>
2020-08-25 20:57:53 +0000

    Add arg parser to cli

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,18 @@
+import argparse
+
 from lib.game import Game
 
+def start_game(args : argparse.Namespace):
+    print('Playing a game with {} players.'.format(args.num_players))
+    game = Game(args.num_players)
+
 def main():
-    game = Game(5)
+    parser = argparse.ArgumentParser(description='Play a game of Romanian Whist.')
+    parser.add_argument('--num_players', type=int, help='Number of players.')
+    args = parser.parse_args()
+
+    start_game(args)
+
 
 if __name__ == '__main__':
     main()
